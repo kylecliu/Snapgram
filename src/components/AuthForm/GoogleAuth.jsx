b2582d9ac9becc994c4b769966c5e24157424dc2
Chun-Hsien Liu <question771118@gmail.com>
2024-09-23 11:59:01 +0800

    fixed logo and added vercel.json

--- src/components/AuthForm/GoogleAuth.jsx
+++ src/components/AuthForm/GoogleAuth.jsx
@@ -8,7 +8,7 @@ const GoogleAuth = () => {
 
   return (
     <HStack>
-        <Image src='public/google.png' h='20px' alt='google icon'></Image>
+        <Image src='google.png' h='20px' alt='google icon'></Image>
         <Text 
         fontSize='sm' 
         color='gray' 
