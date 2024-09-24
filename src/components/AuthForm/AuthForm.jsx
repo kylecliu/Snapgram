5658146b98c687865a51ec80e0a918198c3bd9ea
Chun-Hsien Liu <question771118@gmail.com>
2024-09-24 14:21:04 +0800

    Enhanced mobile login page

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -12,38 +12,38 @@ const AuthForm = () => {
 
   return (
     // Login section
-    <Box>
-    <Box border='2px solid gray' borderRadius={5} padding={5}>
-        <VStack>
-            <Image src={img} alt="logo" w={300} h={100} my={5}/>
+    <>
+        <Box border='2px solid gray' borderRadius={5} padding={5}>
+            <VStack>
+                <Image src={img} alt="logo" w={{base: 240, sm: 300}} h={{base:'80px',sm: 100}} my={5}/>
 
-            { isSignedUp ? <Login /> : <Signup />}
+                { isSignedUp ? <Login /> : <Signup />}
 
 
-            {/* -------OR------- */}
-            <Flex alignItems='center' justify='center' width={'100%'} my={4} gap={1}>
-                <Box border={'1px solid gray'} width={'100%'}/>
-                <Box>OR</Box>
-                <Box border={'1px solid gray'} width={'100%'}/>
-            </Flex>
-            <GoogleAuth />
-        </VStack>
-    </Box>
+                {/* -------OR------- */}
+                <Flex alignItems='center' justify='center' width={'100%'} my={4} gap={1}>
+                    <Box border={'1px solid gray'} width={'100%'}/>
+                    <Box>OR</Box>
+                    <Box border={'1px solid gray'} width={'100%'}/>
+                </Flex>
+                <GoogleAuth />
+            </VStack>
+        </Box>
 
             {/* Sign up or log in */}
-    <Flex 
-    my={2} 
-    border={'2px solid gray'} 
-    borderRadius={4} 
-    padding={5} 
-    justify={'center'} 
-    align={'center'} 
-    marginTop={5}
-    >
-        <Box mx={2} color='gray' fontSize={14}>{isSignedUp ? "Don't have an account?" : "Already have an account?" }</Box>
-        <Box mx={2} color='gray' fontSize={14} cursor={'pointer'} onClick={() => setIsSignedUp(!isSignedUp)}>{isSignedUp ? "Sign Up" : "Log In"}</Box>
-    </Flex>
-    </Box>
+        <Flex 
+        my={2} 
+        border={'2px solid gray'} 
+        borderRadius={4} 
+        padding={5} 
+        justify={'center'} 
+        align={'center'} 
+        marginTop={5}
+        >
+            <Box mx={2} color='gray' fontSize={14}>{isSignedUp ? "Don't have an account?" : "Already have an account?" }</Box>
+            <Box mx={2} color='gray' fontSize={14} cursor={'pointer'} onClick={() => setIsSignedUp(!isSignedUp)}>{isSignedUp ? "Sign Up" : "Log In"}</Box>
+        </Flex>
+    </>
   )
 }
 
